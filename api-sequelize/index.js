fe65969065e4cc5cf8083ba9967388959eace5da
Elie Bismuth <eliebismuth91@gmail.com>
2020-12-30 16:22:49 +0100

    up

--- api-sequelize/index.js
+++ api-sequelize/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 const sequelize = require('./util/database');
-const User = require('./models/user');
 const userRoutes = require('./routes/user');
 
 app.use(express.json());
